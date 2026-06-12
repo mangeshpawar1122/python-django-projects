7e16cb1861ad760425e704f7211afbb0864ca802
mangeshpawar1122 <pawarmangesh112203@gmail.com>
2026-06-12 18:44:30 +0530

    this time i am add a multiples user and create a admin panel and add a data admin side use a admin panel

--- myproject15/portfolio/admin.py
+++ myproject15/portfolio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from portfolio.models import Student,Profile
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Profile)
--- myproject15/portfolio/models.py
+++ myproject15/portfolio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+  name = models.CharField(max_length=100)
+  age = models.IntegerField()
+  city = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+  
+
+class Profile(models.Model):
+  bio = models.TextField(max_length=500)
+  location = models.CharField(max_length=100)
+  birt_date = models.DateField(null=True,blank=True)
+
+  def __str__(self):
+    return str(self.location)
