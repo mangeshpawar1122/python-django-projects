556965014833c20ca36ea01227995272cd0f7b63
mangeshpawar1122 <pawarmangesh112203@gmail.com>
2026-06-12 18:18:36 +0530

    in this project i am creating a superuser and admin panel side add a new user

---
+++ myproject15/portfolio/urls.py
@@ -0,0 +1,6 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.student_list,name='student_list'),
+]
---
+++ myproject15/portfolio/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def student_list(request):
+  return render(request,'portfolio/student_list.html')
